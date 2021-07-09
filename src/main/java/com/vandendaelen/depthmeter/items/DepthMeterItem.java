cae1e56d8274c880dcf3436d5eb7f601e8c53585
Vandendaelen <vandendaelenclement@gmail.com>
2021-07-09 23:12:14 +0200

    Added tooltip on hotbar + fixed visual

--- src/main/java/com/vandendaelen/depthmeter/items/DepthMeterItem.java
+++ src/main/java/com/vandendaelen/depthmeter/items/DepthMeterItem.java
@@ -1,6 +1,7 @@
 package com.vandendaelen.depthmeter.items;
 
 import com.vandendaelen.depthmeter.capabilities.DepthMeterCapabilities;
+import com.vandendaelen.depthmeter.capabilities.IDepth;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -9,7 +10,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
@@ -41,15 +45,24 @@ public class DepthMeterItem extends Item {
     public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
         stack.getCapability(DepthMeterCapabilities.DEPTH).ifPresent(cap -> {
-
-            int posSeaLevel = cap.getPosSeaLevel();
-            if (posSeaLevel >= 0){
-                tooltip.add(new TranslationTextComponent("depthmeter.sea_level_above", String.valueOf(Math.abs(posSeaLevel))));
-            }
-            else{
-                tooltip.add(new TranslationTextComponent("depthmeter.sea_level_below", String.valueOf(Math.abs(posSeaLevel))));
-            }
+            tooltip.add(getDepthInformation(cap).mergeStyle(TextFormatting.GRAY));
         });
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        playerIn.getHeldItem(handIn).getCapability(DepthMeterCapabilities.DEPTH).ifPresent(cap -> {
+            playerIn.sendStatusMessage(getDepthInformation(cap), true);
+        });
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+    public TranslationTextComponent getDepthInformation(IDepth cap){
+        int posSeaLevel = cap.getPosSeaLevel();
+        if (posSeaLevel >= 0){
+            return new TranslationTextComponent("depthmeter.sea_level_above", String.valueOf(Math.abs(posSeaLevel)));
+        }
+        return new TranslationTextComponent("depthmeter.sea_level_below", String.valueOf(Math.abs(posSeaLevel)));
     }
 
     @Override
